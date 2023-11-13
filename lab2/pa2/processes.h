d6481905d77a83d5aafd89a44d7326140bb32727
senina mariya <senina.m.m@yandex.ru>
2023-11-13 13:13:50 +0300

    project builds

---
+++ lab2/pa2/processes.h
@@ -0,0 +1,30 @@
+
+#ifndef __IFMO_DISTRIBUTED_CLASS_PROCESSES__H
+#define __IFMO_DISTRIBUTED_CLASS_PROCESSES__H
+#include "ipc.h"
+#include <stdio.h> 
+#include <sys/types.h>
+#include <unistd.h>
+
+
+typedef struct{
+    int fw;
+    int fr;
+} Pipe;
+
+typedef struct{
+    FILE* processes;
+    FILE* pipes;
+} Log;
+
+typedef struct{
+    local_id id;
+    local_id parent_id;
+    pid_t pid;
+    pid_t parent_pid;
+    int num_of_processes; 
+    Pipe*** pipes;
+    Log* log;
+} Process;
+
+#endif // __IFMO_DISTRIBUTED_CLASS_PROCESSES__H
