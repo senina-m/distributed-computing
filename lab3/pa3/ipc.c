6445fd800e2adab654d37676a90aacdd2a633075
senina mariya <senina.m.m@yandex.ru>
2023-11-15 15:04:00 +0300

    initial commit lab3

---
+++ lab3/pa3/ipc.c
@@ -0,0 +1,153 @@
+#include "ipc.h"
+#include "processes.h"
+#include <errno.h>
+
+int send(void *self, local_id dst, const Message *msg) {
+  Process *this = (Process *)self;
+  local_id src = this->id;
+  size_t msg_len = sizeof(MessageHeader) + msg->s_header.s_payload_len;
+  ssize_t ret = write(this->pipes[dst][src]->fw, msg, msg_len);
+  if (ret == msg_len) {
+    // printf("Send message successfull from %d pipe to %d: %s\n", src, dst,
+    // msg->s_payload);
+    return 0;
+  } else {
+    printf("Can't send message from %d pipe to %d: return %ld, expect %ld\n",
+           src, dst, ret, msg_len);
+    perror("Can't send message");
+    return -1;
+  }
+}
+
+int send_multicast(void *self, const Message *msg) {
+  Process *this = (Process *)self;
+  local_id src = this->id;
+  for (int i = 0; i < this->num_of_processes; i++) {
+    if (i != src) {
+      if (send(this, i, msg)) {
+        printf("Can't send multicast message from %d\n", src);
+      }
+    }
+  }
+  return 0;
+}
+
+int receive(void *self, local_id from, Message *msg) {
+  Process *this = (Process *)self;
+  int listened_pipe = this->pipes[this->id][from]->fr;
+  uint8_t is_read_header = 0;
+  size_t bytes_to_read = sizeof(MessageHeader);
+  while (1) {
+    ssize_t read_count = 0;
+    if (!is_read_header) {
+      read_count = read(listened_pipe,
+                        &msg->s_header + sizeof(MessageHeader) - bytes_to_read,
+                        bytes_to_read);
+      if (read_count == bytes_to_read) {
+        is_read_header = 1;
+        bytes_to_read = msg->s_header.s_payload_len;
+        // printf("Read header from %d pipe to %d successfull: type %d,
+        // size_payload %d\n", from, this->id, msg->s_header.s_type,
+        // msg->s_header.s_payload_len);
+      } else if (read_count > 0) {
+        bytes_to_read -= read_count;
+      } else if (errno != EAGAIN) {
+        printf("Can't read from %d pipe to %d\n", from, this->id);
+        perror("Can't receive header");
+        return -1;
+      }
+    }
+    if (is_read_header) {
+      read_count =
+          read(listened_pipe,
+               msg->s_payload + msg->s_header.s_payload_len - bytes_to_read,
+               bytes_to_read);
+      if (read_count == bytes_to_read) {
+        msg->s_payload[msg->s_header.s_payload_len] = 0;
+        // printf("Receive message from %d pipe to %d successfull: %s\n", from,
+        // this->id, msg->s_payload);
+        return 0;
+      } else if (read_count > 0) {
+        bytes_to_read -= read_count;
+      } else if (errno != EAGAIN) {
+        printf("Can't read from %d pipe to %d\n", from, this->id);
+        perror("Can't receive payload");
+        return -1;
+      }
+    }
+  }
+}
+
+int receive_any(void *self, Message *msg) {
+  Process *this = (Process *)self;
+  uint8_t is_chose_pipe = 0;
+  uint8_t is_read_header = 0;
+  local_id from = -1;
+  size_t bytes_to_read = sizeof(MessageHeader);
+  while (1) {
+    // printf("DEBUG %i: try to read_any\n", this->id);
+    ssize_t read_count = 0;
+    if (!is_chose_pipe) {
+      for (int i = 0; i < this->num_of_processes; i++) {
+        if (i != this->id) {
+          // printf("DEBUG %i: do read from %i\n", this->id, i);
+          read_count = read(this->pipes[this->id][i]->fr, &msg->s_header,
+                            sizeof(MessageHeader));
+          // printf("DEBUG %i: do read from %i bytes=%i\n", this->id, i, read_count);
+          if (read_count == bytes_to_read) {
+            // printf("DEBUG %i: header from proc %i\n", this->id, i);
+            is_chose_pipe = 1;
+            from = i;
+            is_read_header = 1;
+            bytes_to_read = msg->s_header.s_payload_len;
+            break;
+          } else if (read_count > 0) {
+            // printf("DEBUG %i: read %i bytes from proc %i\n", this->id, read_count, i);
+            is_chose_pipe = 1;
+            from = i;
+            bytes_to_read -= read_count;
+            // printf("DEBUG %i: is_chouse_pipe=%i from=%i bytes_to_read=%i is_read_header=%i\n", this->id, is_chose_pipe, from, bytes_to_read, is_read_header);
+            break;
+          } else if (errno != EAGAIN) {
+            printf("Can't read any to %d pipe\n", this->id);
+            return -1;
+          }
+        }
+      }
+    }
+    // printf("DEBUG %i: is_chose_pipe=%i is_read_header=%i\n", this->id, is_chose_pipe, is_read_header);
+    if (is_chose_pipe && !is_read_header) {
+      // printf("DEBUG %i: read choose pipe and didn't finished read\n", this->id);
+      read_count = read(this->pipes[this->id][from]->fr,
+                        &msg->s_header + sizeof(MessageHeader) - bytes_to_read,
+                        bytes_to_read);
+      if (read_count == bytes_to_read) {
+        // printf("DEBUG %i: finished reading header\n", this->id);
+        is_read_header = 1;
+        bytes_to_read = msg->s_header.s_payload_len;
+      } else if (read_count > 0) {
+        // printf("DEBUG %i: didn't finished reading header\n", this->id);
+        bytes_to_read -= read_count;
+      } else if (errno != EAGAIN) {
+        printf("Can't read from %d pipe to %d\n", from, this->id);
+        return -1;
+      }
+    }
+    if (is_chose_pipe && is_read_header) {
+      read_count =
+          read(this->pipes[this->id][from]->fr,
+               msg->s_payload + msg->s_header.s_payload_len - bytes_to_read,
+               bytes_to_read);
+      if (read_count == bytes_to_read) {
+        msg->s_payload[msg->s_header.s_payload_len] = 0;
+        // printf("DEBUG %i: Receive message from %d pipe to %d successfull\n", this->id, from, this->id);
+        return 0;
+      } else if (read_count > 0) {
+        bytes_to_read -= read_count;
+      } else if (errno != EAGAIN) {
+        printf("Can't read from %d pipe to %d\n", from, this->id);
+        return -1;
+      }
+    }
+  }
+}
---
+++ lab3/pa3/pipes.c
@@ -0,0 +1,78 @@
+
+#include "pipes.h"
+
+
+Pipe*** alloc_pipes(int n){
+    Pipe*** pipes = (Pipe***)malloc(sizeof(Pipe**)*n);
+    for (int i = 0; i < n; i++){
+        pipes[i] = (Pipe**)malloc(sizeof(Pipe*)*n);
+        for(int j = 0; j < n; j++){
+            pipes[i][j] = mmalloc(Pipe);
+        }
+    }
+    return pipes;
+}
+
+void free_pipes(Pipe*** ptr, int n){
+    for (int i = 0; i < n; i++){
+        free(ptr[i]);
+    }
+    free(ptr);
+}
+
+int init_pipes(Process* this){
+    // printf("INITING PIPES\n");
+    int n = this->num_of_processes;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if(i != j){
+                int fd[2];
+                if (pipe(fd) == 0){
+                    this->pipes[i][j]->fr = fd[0];
+                    this->pipes[i][j]->fw = fd[1];
+                    fcntl( fd[0], F_SETFL, O_NONBLOCK);
+                    fcntl( fd[1], F_SETFL, O_NONBLOCK);
+                    fprintf(this->log->pipes, "Process %i opens pipe %i -> %i, with descriptors r:%i, w:%i\n", this->id, i, j, fd[0], fd[1]);
+                    // printf("Process %i opens pipe %i -> %i, with descriptors r:%i, w:%i\n", this->id, i, j, fd[0], fd[1]);
+                }else{
+                    printf("Can't create pipes!\n");
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int close_unused_pipes(Process* this){
+    for (int i = 0; i < this->num_of_processes; i++) {
+        for (int j = 0; j < this->num_of_processes; j++) {
+            if ( i != this->id && i != j ) {
+                if((close(this->pipes[i][j]->fr) == 0) && (close(this->pipes[j][i]->fw) == 0)){
+                    fprintf(this->log->pipes, "Process %i closed pipes r:%i %i and w:%i %i\n", this->id, i, j, j, i);
+                    // printf("Process %i closed pipes r:%i %i and w:%i %i\n", this->id, i, j, j, i);
+                }else{
+                    fprintf(this->log->pipes, "Process %i CAN'T close pipes r:%i %i and w:%i %i\n", this->id, i, j, j, i);
+                    return 1;
+                }
+            }
+        }
+    } 
+    return 0;
+}
+
+int close_used_pipes(Process* this){
+    for (int i = 0; i < this->num_of_processes; i++) {
+        for (int j = 0; j < this->num_of_processes; j++) {
+            if (i == this->id && i != j) {
+                if((close(this->pipes[i][j]->fr) == 0) && (close(this->pipes[j][i]->fw) == 0)){
+                    // fprintf(this->log->pipes, "Process %i closed used pipes r:%i %i and w:%i %i\n", this->id, i, j, j, i);
+                }else{
+                    fprintf(this->log->pipes, "Process %i CAN'T close used pipes r:%i %i and w:%i %i\n", this->id, i, j, j, i);
+                    return 1;
+                }
+            }
+        }
+    } 
+    return 0;
+}
